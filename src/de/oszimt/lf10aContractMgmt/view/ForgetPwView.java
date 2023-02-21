5288219b101d93eed9ec06f0e390745c26f272e4
Benny <empty>
2023-02-21 11:48:04 +0100

    add pw forget field

--- src/de/oszimt/lf10aContractMgmt/view/ForgetPwView.java
+++ src/de/oszimt/lf10aContractMgmt/view/ForgetPwView.java
@@ -11,19 +11,18 @@ import java.util.Date;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class LoginView extends JFrame {
+public class ForgetPwView extends JFrame {
 
 	private JLabel usernameLabel, passwordLabel, time;
 	private JTextField usernameField;
 	private JPasswordField passwordField;
-	private JButton loginButton, forgetPw;
-	private	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+	private JButton saveBtn, cancelBtn;
 
-	//Hardcoded login data
+	// Hardcoded login data
 	private String enteredPw = "Test123";
 	private String enteredName = "Test";
 
-	public LoginView(HaseGmbHManagement driver) {
+	public ForgetPwView(HaseGmbHManagement driver) {
 		setResizable(false);
 		setTitle("Login");
 		setSize(800, 500);
@@ -35,65 +34,71 @@ public class LoginView extends JFrame {
 		GridBagConstraints constraints = new GridBagConstraints();
 		constraints.insets = new Insets(5, 5, 5, 5);
 
-		usernameLabel = new JLabel("Username:");
+		usernameLabel = new JLabel("aktuellen Benutzernamen eingeben:");
 		constraints.gridx = 0;
 		constraints.gridy = 0;
 		panel.add(usernameLabel, constraints);
 
 		usernameField = new JTextField(10);
-		constraints.gridx = 1;
-		constraints.gridy = 0;
+		constraints.gridx = 0;
+		constraints.gridy = 1;
 		usernameField.setText("Test");
 		panel.add(usernameField, constraints);
 
-		passwordLabel = new JLabel("Password:");
+		passwordLabel = new JLabel("neues Passwort eingeben:");
 		constraints.gridx = 0;
-		constraints.gridy = 1;
+		constraints.gridy = 2;
+		panel.add(passwordLabel, constraints);
+
+		passwordLabel = new JLabel("neues Passwort eingeben:");
+		constraints.gridx = 0;
+		constraints.gridy = 2;
 		panel.add(passwordLabel, constraints);
 
 		passwordField = new JPasswordField(10);
-		passwordField.setText("Test123");
-		constraints.gridx = 1;
-		constraints.gridy = 1;
-		panel.add(passwordField, constraints);
-
-		loginButton = new JButton("Login");
-		constraints.gridx = 0;
-		constraints.gridy = 2;
-		constraints.gridwidth = 2;
-		constraints.anchor = GridBagConstraints.CENTER;
-		panel.add(loginButton, constraints);
-
-		forgetPw = new JButton("Passwort vergessen");
+		passwordField.setText("");
 		constraints.gridx = 0;
 		constraints.gridy = 3;
+		panel.add(passwordField, constraints);
+
+		saveBtn = new JButton("Save");
+		constraints.gridx = 0;
+		constraints.gridy = 4;
 		constraints.gridwidth = 2;
 		constraints.anchor = GridBagConstraints.CENTER;
-		panel.add(forgetPw, constraints);
-		
+		panel.add(saveBtn, constraints);
 
-		loginButton.addActionListener(new ActionListener() {
+		cancelBtn = new JButton("Cancel");
+		constraints.gridx = 0;
+		constraints.gridy = 5;
+		constraints.gridwidth = 2;
+		constraints.anchor = GridBagConstraints.CENTER;
+		panel.add(cancelBtn, constraints);
+
+		cancelBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (checkLoginInserts()) {
-					new Overview(driver).setVisible(true);
-					dispose(); 
+				new LoginView(driver).setVisible(true);
+				dispose();
+			}
+		});
+
+		saveBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (checkPwResetInserts()) {
+					newPw = String.valueOf(passwordField.getPassword());
+					new LoginView(driver).setVisible(true);
+					dispose();
 				}
 			}
 		});
 
-		forgetPw.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				new ForgetPwView(driver).setVisible(true);
-				dispose(); 
-			}
-		});
 		add(panel);
 		setVisible(true);
 	}
 
-	private boolean checkLoginInserts() {
+	private boolean checkPwResetInserts() {
 		String passwordValue = String.valueOf(passwordField.getPassword());
 		String usernameValue = usernameField.getText();
 		if (usernameField.getText().isBlank()) {
@@ -101,15 +106,20 @@ public class LoginView extends JFrame {
 			return false;
 		}
 		if (passwordField.getPassword().length == 0) {
-			JOptionPane.showMessageDialog(null, "Passworteingabefeld ist leer!", "Fehler", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Passwordfeld ist leer!", "Fehler", JOptionPane.ERROR_MESSAGE);
 			return false;
 		}
-		if (!(passwordValue.equals(enteredPw) && usernameValue.equals(enteredName))) {
-			JOptionPane.showMessageDialog(null, "Falsches Passwort oder Benutzername. Bitte versuchen Sie es erneut.",
-					"Fehler", JOptionPane.ERROR_MESSAGE);
+		if (passwordValue.equals(enteredPw)) {
+			JOptionPane.showMessageDialog(null, "Das Passwort ähnelt dem vorherigem Passwort!", "Fehler",
+					JOptionPane.ERROR_MESSAGE);
 			return false;
 		}
-
+		if (!(usernameValue.equals(enteredName))) {
+			JOptionPane.showMessageDialog(null, "Benutzername ist nicht korrekt!", "Fehler", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		JOptionPane.showMessageDialog(null, "Passwort wurde erfolgreich geändert. Sie werden nun zur Loginübersicht weitergeleitet!", "Erfolgreich",
+				JOptionPane.INFORMATION_MESSAGE);
 		return true;
 	}
 }
